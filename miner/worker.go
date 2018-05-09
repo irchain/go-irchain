90d2ca40c53d78af0049eac7671eed6c541fc069
ldcc <ldcc@live.com>
2018-05-09 20:44:07 +0800

    debug

--- miner/worker.go
+++ miner/worker.go
@@ -458,25 +458,25 @@ func (self *worker) commitNewWork(isFirstWork bool) {
 	}
 
 	// Waiting for transaction, until any transactions found
-	for {
+	// for {
 		if pending, err := self.huc.TxPool().Pending(); err != nil {
 			log.Error("Failed to fetch pending transactions", "err", err)
 			return
-		} else if atomic.LoadInt32(&self.mining) == 0 {
-			log.Trace("Stop mining, interrupt the loops", "block num", num.Int64())
-			return
-		} else if isFirstWork && len(pending) == 0 && num.Cmp(common.Big1) == 1 {
-			log.Trace("Sleep mining, waiting for transactions", "pending num", len(pending))
-			self.mu.Unlock()
-			time.Sleep(txsRefreshSec * time.Second)
-			self.mu.Lock()
-			continue
+		// } else if atomic.LoadInt32(&self.mining) == 0 {
+		// 	log.Trace("Stop mining, interrupt the loops", "block num", num.Int64())
+		// 	return
+		// } else if isFirstWork && len(pending) == 0 && num.Cmp(common.Big1) == 1 {
+		// 	log.Trace("Sleep mining, waiting for transactions", "pending num", len(pending))
+		// 	self.mu.Unlock()
+		// 	time.Sleep(txsRefreshSec * time.Second)
+		// 	self.mu.Lock()
+		// 	continue
 		} else {
 			txs := types.NewTransactionsByPriceAndNonce(work.signer, pending)
 			work.commitTransactions(self.mux, txs, self.chain, self.coinbase)
-			break
+			// break
 		}
-	}
+	// }
 
 	// compute uncles for the new block.
 	var (
